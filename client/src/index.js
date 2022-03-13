5ccc0274089dc8d6b977d887691e2179be830d5e
matisses goldfish <honoregn@gmail.com>
2022-03-13 15:47:05 -0500

    added App, as well as code for apollo server and graphql, updated some code in the server, and currently working on mutations

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,3 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
